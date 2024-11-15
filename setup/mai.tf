766325ce9f8c859540c4c297790a266aad630572
mizuki4i4 <onogawa.mizuki@gmail.com>
2024-11-15 17:02:37 +0900

    add: setup

---
+++ setup/mai.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "tam-workbench-creator-state-bucket"
+    prefix = "mizuki-demo-joonix"
+  }
+}
+
+resource "google_storage_bucket" "tam_workbench_creator" {
+  name          = "tam-workbench-creator-upload-bucket"
+  project = "mizuki-demo-joonix" 
+  location      = "asia-northeast1"
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+}
