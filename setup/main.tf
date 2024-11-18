e313b9f9e7c28b8f08216bd7702ac0563a6ac2d4
mizuki4i4 <onogawa.mizuki@gmail.com>
2024-11-18 16:51:25 +0900

    Update Terraform code using VertexAI -> Upload file: ${filePath}-${Date.now()}

--- setup/main.tf
+++ setup/main.tf
@@ -25,3 +25,29 @@ resource "google_storage_bucket" "tam_workbench_creator" {
     enabled = true
   }
 }
+
+resource "google_compute_instance" "vm1" {
+  project      = "mizuki-demo-joonix"
+  zone         = "asia-northeast1-a" 
+  machine_type = "e2-standard-2"  
+  name         = "vm1"
+ network_interface {
+    subnetwork = "default" 
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+}
+
+
+
+resource "google_storage_bucket" "default" {
+  project = "mizuki-demo-joonix"
+  name                        = "mizuki-demo-joonix-gcs-bucket"
+  location = "ASIA-NORTHEAST1"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+}
