8538e946fa23a4be529e39eb94723cccd8093d00
mizuki4i4 <onogawa.mizuki@gmail.com>
2024-11-19 00:57:43 +0900

    Update Terraform code using VertexAI -> Upload file: ${filePath}-${Date.now()}

--- setup/main.tf
+++ setup/main.tf
@@ -25,3 +25,46 @@ resource "google_storage_bucket" "tam_workbench_creator" {
     enabled = true
   }
 }
+
+# Configure the Google Cloud provider
+
+provider "google" {
+  project = "mizuki-demo-joonix"
+  region  = "asia-northeast1"
+}
+
+# GKE Cluster
+resource "google_container_cluster" "primary" {
+  name     = "mizuki-demo-joonix-cluster"
+  location = "asia-northeast1"
+
+  network    = "default"
+  subnetwork = "default"
+
+ initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium" # Example machine type
+  }
+}
+
+
+
+# Cloud SQL for Postgres
+resource "google_sql_database_instance" "default" {
+  name             = "mizuki-demo-joonix-postgres"
+  region           = "asia-northeast1"
+  database_version = "POSTGRES_14" # Example Postgres version
+  settings {
+    tier = "db-f1-micro" # Example tier
+  }
+  deletion_protection = false # Allows for destroying in Terraform
+}
+
+# GCS Bucket
+resource "google_storage_bucket" "default" {
+  name                        = "mizuki-demo-joonix-storage"
+  location                    = "asia-northeast1"
+  storage_class               = "STANDARD"
+ uniform_bucket_level_access = true
+}
